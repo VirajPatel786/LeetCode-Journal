26c84f8d857d42127e2135354fb34f738eec09ed
Viraj Patel <86842256+VirajPatel786@users.noreply.github.com>
2024-08-28 13:09:45 -0600

    Time: 414 ms (57.94%), Space: 63.8 MB (13.3%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* replaceElements(int* arr, int arrSize, int* returnSize) {
+    *returnSize = arrSize;
+    int* result = (int*)malloc(arrSize * sizeof(int));
+    int largest = -1;
+    for (int i = arrSize - 1; i >= 0; i--) {
+        result[i] = largest;
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return result;
+}
