0d5a7111a02c29075bb7ddef7f536d7c6877b05e
Viraj Patel <86842256+VirajPatel786@users.noreply.github.com>
2024-10-09 14:29:58 -0600

    Time: 36 ms (64.73%), Space: 17.1 MB (91.82%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -14,44 +14,47 @@ class Solution:
         Flattens a multilevel doubly linked list into a single-level doubly linked list.
 
         Parameters:
-        head (Node): The head of the multilevel doubly linked list.
+        head (Optional[Node]): The head of the multilevel doubly linked list.
 
         Returns:
-        Node: The head of the flattened doubly linked list.
+        Optional[Node]: The head of the flattened doubly linked list.
         """
+        current_node = head  # Pointer to the current node we are traversing
         
-        if not head:
-            return None  # If the list is empty, return None
-        
-        current = head  # Start at the head of the list
-        
-        while current:
-            if current.child:
-                # Flatten the child list recursively
-                child = current.child
-                next_node = current.next  # Save the next node
-                
-                # Flatten the child list and attach it to the current node
-                flattened_child = self.flatten(child)
-                
-                # Connect current node to the child list
-                current.next = flattened_child
-                flattened_child.prev = current
-                
-                # Find the tail of the flattened child list to reconnect the rest
-                tail = flattened_child
-                while tail.next:
-                    tail = tail.next  # Move to the end of the flattened child list
-                
-                # Connect the tail of the flattened child list to the next node
-                tail.next = next_node
-                if next_node:
-                    next_node.prev = tail  # If next_node exists, connect its prev to the tail
-                
-                # Clear the child pointer as we've flattened it
-                current.child = None
+        while current_node is not None:
+            # If the current node has a child, we need to flatten the child list into the main list
+            if current_node.child is not None:
+                # Merge the child list starting from the current node
+                self._merge_child_list(current_node)
             
-            # Move to the next node in the list
-            current = current.next
+            # Move to the next node in the parent list
+            current_node = current_node.next
         
-        return head  # Return the head of the flattened list
+        return head
+
+    def _merge_child_list(self, current_node: 'Node') -> None:
+        """
+        Merges the child list into the main doubly linked list at the current node.
+
+        Parameters:
+        current_node (Node): The node in the parent list that has a child list to be merged.
+        """
+        child_list_head = current_node.child  # The start of the child list
+        
+        # Traverse to the end of the child list to get the last node
+        child_list_tail = child_list_head
+        while child_list_tail.next is not None:
+            child_list_tail = child_list_tail.next
+        
+        # If the current node has a next node, connect the tail of the child list to it
+        if current_node.next is not None:
+            child_list_tail.next = current_node.next  # Connect the child list tail to the current next node
+            current_node.next.prev = child_list_tail  # Update the prev pointer of the next node
+        
+        # Now connect the current node to the child list's head
+        current_node.next = current_node.child  # Link current node to the start of the child list
+        current_node.child.prev = current_node  # Update the prev pointer of the child list head
+
+        # Remove the child pointer as the child list has been flattened
+        current_node.child = None
+        
