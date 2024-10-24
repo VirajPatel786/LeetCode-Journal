1b7175f532dff90099b3191867b9279b7c1cdf6f
VirajPatel-99 <86842256+VirajPatel786@users.noreply.github.com>
2024-10-24 16:34:00 -0600

    Relocating

--- Explore Card Track/3. Linked List/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
+++ Explore Card Track/3. Linked List/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
@@ -5,16 +5,29 @@
  *     struct ListNode *next;
  * };
  */
+
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    // If either list is empty, there is no intersection
     if ((headA == NULL) || (headB == NULL)) {
         return NULL;
     }
-    struct ListNode *pointerA = headA, *pointerB = headB;
 
+    // Initialize two pointers, starting at the heads of each list
+    struct ListNode *pointerA = headA;
+    struct ListNode *pointerB = headB;
+
+    // Traverse through both lists
     while (pointerA != pointerB) {
+        // Move pointerA to the next node in list A,
+        // or to the head of list B if pointerA has reached the end of list A
         pointerA = (pointerA != NULL) ? pointerA->next : headB;
+
+        // Move pointerB to the next node in list B,
+        // or to the head of list A if pointerB has reached the end of list B
         pointerB = (pointerB != NULL) ? pointerB->next : headA;
     }
 
+    // When pointerA equals pointerB, they have either met at the intersection node
+    // or both are NULL (meaning there is no intersection)
     return pointerA;
 }
